902c76155a5d551840e9faec9cfe88bd4725eef7
BaltazarBX <cristobalchaperon.aq@gmail.com>
2023-11-02 18:09:15 -0300

    Trabajo listo

--- app/src/main/java/com/example/sqlitecrud2/daoContacto.java
+++ app/src/main/java/com/example/sqlitecrud2/daoContacto.java
@@ -9,18 +9,15 @@ import java.util.ArrayList;
 
 public class daoContacto {
 
-    SQLiteDatabase bd;//Instancia de la base de datos
-    ArrayList<Contacto>lista = new ArrayList<Contacto>();//Almacena una lista de  objetos de tipo contacto
-    Contacto c;//Instancia de la clase contacto, utilizada para operaciones de busqueda
-    Context ct;//contexto de la aplicacion
-    String nombreBD= "BDContactos";//nombre de la Base de datos
-
-    //cadena de sentencia sql que crea la tabla contacto si no existe, define las columnas de las tablas
+    SQLiteDatabase bd;
+    ArrayList<Contacto>lista = new ArrayList<Contacto>();
+    Contacto c;
+    Context ct;
+    String nombreBD= "BDContactos";
     String tabla = "create table if not exists contacto(id integer primary key autoincrement, nombre text, apellido text, email text, telefono text, ciudad text)";
 
 
     public daoContacto(Context c){
-        //Constructor de la clase, acepta un parametro de tipo context y utiliza para inicializar la clase
         this.ct=c;
         bd=c.openOrCreateDatabase(nombreBD, Context.MODE_PRIVATE, null);
         bd.execSQL(tabla);
@@ -28,7 +25,6 @@ public class daoContacto {
     }
 
     public boolean insertar(Contacto c){
-        //metodo para insertar un nuevo contacto en la base de datos
         ContentValues contenedor = new ContentValues();
         contenedor.put("nombre", c.getNombre());
         contenedor.put("apellido", c.getApellido());
@@ -39,12 +35,10 @@ public class daoContacto {
     }
 
     public boolean eliminar(int id){
-        //metodo para eliminar el contacto de la BD segun el id proporcionado,
         return (bd.delete("contacto","id="+id,null ))>0;
     }
 
     public boolean editar(Contacto c){
-        //metodo para actualizar la informacion de un contacto en la base de datos
         ContentValues contenedor = new ContentValues();
         contenedor.put("nombre", c.getNombre());
         contenedor.put("apellido", c.getApellido());
@@ -55,7 +49,6 @@ public class daoContacto {
     }
 
     public ArrayList<Contacto>verTodo(){
-        //Metodo para recuperar una lista de contacto desde la base de datos
         lista.clear();
         Cursor cursor = bd.rawQuery("select * from contacto", null);
         if (cursor!=null && cursor.getCount()>0){
@@ -71,7 +64,6 @@ public class daoContacto {
     }
 
     public Contacto verUno(int posicion){
-        //Metodo para buscar el contacto segun su id en la base de datos y devolverlo como un objeto contacto.
         Cursor cursor = bd.rawQuery("select * from contacto", null);
         cursor.moveToPosition(posicion);
         c=new Contacto(cursor.getInt(0),
